5ad49997727739baea5b7d4554ee482218ad8f31
Tumin Sheth <tumin.sheth102987@marwadiuniversity.ac.in>
2020-06-18 03:00:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
