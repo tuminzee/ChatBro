d0cfc23d4352cf770de29e0e3f6223e834a692ed
Tumin Sheth <tumin.sheth102987@marwadiuniversity.ac.in>
2020-06-18 11:18:41 +0530

    new connection notification setup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Run when a client connects 
+io.on('connection', socket => {
+    console.log('New Socket Connection');
+});
 
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
 });
